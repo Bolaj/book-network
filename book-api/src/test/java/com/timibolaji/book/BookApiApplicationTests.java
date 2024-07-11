ee301bb725433c51bf79e99d4af24fa823aa4c49
TimiBolaji <mattybolaji@gmail.com>
2024-07-11 23:18:17 +0100

    first commit

---
+++ book-api/src/test/java/com/timibolaji/book/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timibolaji.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
