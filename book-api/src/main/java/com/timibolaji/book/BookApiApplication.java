af5820dae5c37052c63aba4856b43155dffe1451
TimiBolaji <mattybolaji@gmail.com>
2024-07-11 23:50:46 +0100

    feat: added EnableJpaAuditing

--- book-api/src/main/java/com/timibolaji/book/BookApiApplication.java
+++ book-api/src/main/java/com/timibolaji/book/BookApiApplication.java
@@ -2,8 +2,10 @@ package com.timibolaji.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookApiApplication {
 
 	public static void main(String[] args) {
