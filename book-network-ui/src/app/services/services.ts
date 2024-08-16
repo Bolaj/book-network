d4cff7f9d26c4a5c0a786632d62bd172494d0c85
TimiBolaji <mattybolaji@gmail.com>
2024-08-16 20:28:26 +0100

    fitx: -- deleted frontend

--- book-network-ui/src/app/services/services.ts
+++
@@ -1,3 +0,0 @@
-export { BookService } from './services/book.service';
-export { FeedbackService } from './services/feedback.service';
-export { AuthenticationService } from './services/authentication.service';
